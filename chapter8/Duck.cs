c926326690ddbb7511fe03597c5cdbe810ba8b7b
Jack Wilson <battlejackmaster@gmail.com>
2018-07-20 15:19:26 -0700

    implemented Icomparable interface

--- chapter8/Duck.cs
+++ chapter8/Duck.cs
@@ -6,10 +6,26 @@ using System.Threading.Tasks;
 
 namespace chapter8
 {
-    class Duck
+    class Duck : IComparable<Duck>
     {
         public int size;
         public KindOfDuck Kind;
+
+        public int CompareTo(Duck other)
+        {
+            if(this.size > other.size)
+            {
+                return 1;
+            }
+            else if (this.size < other.size)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
     }
 
     enum KindOfDuck
