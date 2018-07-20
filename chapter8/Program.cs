1f412c1a4cbeebc0797708f49ac65f8e2a465ee3
Jack Wilson <battlejackmaster@gmail.com>
2018-07-20 15:25:22 -0700

    print off sorted duck objects

--- chapter8/Program.cs
+++ chapter8/Program.cs
@@ -12,12 +12,19 @@ namespace chapter8
         {
             List<Duck> ducks = new List<Duck>() {
                 new Duck() { Kind=KindOfDuck.Mallard, size = 17},
-                new Duck() { Kind = KindOfDuck.Decoy, size = 17 },
-                new Duck() { Kind = KindOfDuck.Mallard, size = 17 },
-                new Duck() { Kind = KindOfDuck.Muscovy, size = 17 },
-                new Duck() { Kind = KindOfDuck.Mallard, size = 17 },
+                new Duck() { Kind = KindOfDuck.Decoy, size = 12 },
+                new Duck() { Kind = KindOfDuck.Mallard, size = 22 },
+                new Duck() { Kind = KindOfDuck.Muscovy, size = 11 },
+                new Duck() { Kind = KindOfDuck.Mallard, size = 13 },
             };
 
+            ducks.Sort();
+            foreach(Duck d in ducks)
+            {
+                Console.WriteLine(d.size);
+            }
+
+            Console.ReadKey();
         }
     }
 }
