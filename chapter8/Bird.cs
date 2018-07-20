98f0c5bedd9b43782809ce287531a7d5e2154d83
Jack Wilson <battlejackmaster@gmail.com>
2018-07-20 15:37:09 -0700

    Filled out classes with overrides

---
+++ chapter8/Bird.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapter8
+{
+    class Bird
+    {
+        public string Name { get; set; }
+        public virtual void Fly()
+        {
+            Console.WriteLine("flap, flap");
+
+        }
+        public override string ToString()
+        {
+            return "A bird named " + Name;
+        }
+    }
+}
