5e14f4537ac30ceef2793acda7c1a15db94fde83
Jack Wilson <battlejackmaster@gmail.com>
2018-07-20 15:15:05 -0700

    Add project files.

---
+++ chapter8/Duck.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapter8
+{
+    class Duck
+    {
+        public int size;
+        public KindOfDuck Kind;
+    }
+
+    enum KindOfDuck
+    {
+        Mallard,
+        Muscovy,
+        Decoy,
+    }
+
+
+}
